8ed2d06f5642911e900761dce586b4808346dfdd
Francisco Neto <netofrancisco.pro@gmail.com>
2024-10-23 18:47:51 -0300

    última linha em branco

--- python-beecrowd/p014.py
+++ python-beecrowd/p014.py
@@ -3,4 +3,3 @@
 X = int(input())    # distância total percorrida
 Y = float(input())  # total de combustível gasto
 print(f'{X / Y:.3f} km/l')
-''
--- python-beecrowd/p059-v2.py
+++ python-beecrowd/p059-v2.py
@@ -4,3 +4,4 @@ n = 0
 while n < 100:
     n += 2
     print(n)
+    
