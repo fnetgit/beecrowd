d91fe605b5d759564c2d7eca76343d7f3482fbf2
Francisco Neto <netofrancisco.pro@gmail.com>
2024-10-30 22:26:08 -0300

    p040 feito

---
+++ python-beecrowd/p040.py
@@ -0,0 +1,26 @@
+num = input().split()
+n1 = float(num[0])
+n2 = float(num[1])
+n3 = float(num[2])
+n4 = float(num[3])
+
+media = (n1 * 2 + n2 * 3 + n3 * 4 + n4 * 1) / 10
+print(f'Media: {media:.1f}')
+if media >= 7.0:
+    print('Aluno aprovado.')
+    exit()
+
+elif media < 5.0:
+    print('Aluno reprovado.')
+    exit()
+
+else:  
+    print('Aluno em exame.')
+    exame = float(input(''))
+    final = (exame + media) / 2
+    print(f'Nota do exame: {exame:.1f}')
+if final >=5.0:
+    print('Aluno aprovado.')
+else:
+    print('Aluno reprovado.')
+print(f'Media final: {final:.1f}')
