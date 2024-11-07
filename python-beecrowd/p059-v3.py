bb6b29b843709120275e9c6dcbd2a62ce899fac1
Francisco Neto <netofrancisco.pro@gmail.com>
2024-11-06 21:55:43 -0300

    p66 e 59-v3

---
+++ python-beecrowd/p059-v3.py
@@ -0,0 +1,5 @@
+# Números Pares
+
+for i in range(1,101):
+    if i % 2 == 0:
+        print(i)
---
+++ python-beecrowd/p066.py
@@ -0,0 +1,22 @@
+# Pares, Ímpares, Positivos e Negativos
+
+even = 0 # contadores
+odd = 0
+negatives = 0
+positives = 0
+for i in range(5):
+    number = int(input())
+    if number % 2 == 0:
+        even += 1
+    else:
+        odd  += 1
+
+    if number > 0:
+        positives += 1
+    elif number < 0:
+        negatives += 1
+
+print(f'{even} valor(es) par(es)')
+print(f'{odd} valor(es) impar(es)')
+print(f'{positives} valor(es) positivo(s)')
+print(f'{negatives} valor(es) negativo(s)')
