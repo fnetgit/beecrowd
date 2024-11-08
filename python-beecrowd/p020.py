662d672a318015aeb06c6b11d2826095183853ec
Francisco Neto <netofrancisco.pro@gmail.com>
2024-11-07 22:35:20 -0300

    correção 020

--- python-beecrowd/p020.py
+++ python-beecrowd/p020.py
@@ -5,4 +5,6 @@ year = total_days // 365
 remaining_days = total_days % 365
 month = remaining_days // 30
 days = remaining_days % 30
-print(year,month,days)
+print(f'{year} ano(s)\n'
+      f'{month} mes(es)\n'
+      f'{days} dia(s)')
