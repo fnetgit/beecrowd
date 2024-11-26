4b5822478b5ae511e1d5d1410dbdec00a8328a2f
Francisco Neto <netofrancisco.pro@gmail.com>
2024-11-25 23:53:36 -0300

    1131 e 1015 v1 e v2

---
+++ python-beecrowd/p1015-v2.py
@@ -0,0 +1,9 @@
+# Distância Entre Dois Pontos
+
+p1 = (input()).split()
+p2 = (input()).split()
+x1, y1= map(float, p1)
+x2, y2= map(float, p2)
+
+distance = ((x2 - x1)**2 + (y2 - y1)**2) ** 0.5
+print(f'{distance:.4f}')
---
+++ python-beecrowd/p1131.py
@@ -0,0 +1,34 @@
+# Grenais
+
+new_grenal = 1
+matches = victories_inter = victories_gremio = tie = 0
+
+while new_grenal == 1:
+    goals = (input()).split()
+
+    inter = int(goals[0])
+    gremio = int(goals[1])
+
+    matches += 1
+
+    if inter > gremio:
+        victories_inter += 1
+    elif gremio > inter:
+        victories_gremio += 1
+    elif inter == gremio:
+        tie += 1
+
+    new_grenal = int(input('Novo grenal (1-sim 2-nao)\n'))
+
+    if new_grenal == 2:
+        break
+
+print(f'{matches} grenais')
+print(f'Inter:{victories_inter}')
+print(f'Gremio:{victories_gremio}')
+print(f'Empates:{tie}')
+
+if victories_inter > victories_gremio:
+    print(f'Inter venceu mais')
+elif victories_gremio > victories_inter:
+    print(f'Gremio venceu mais')
