7b71603421ea2b3fdbf9fd6a6ff11ea69c154bf4
Francisco Neto <netofrancisco.pro@gmail.com>
2024-12-01 22:31:40 -0300

    1067 v1 e v2 feitos

---
+++ python-beecrowd/p1067-v1.py
@@ -0,0 +1,17 @@
+# Números Ímpares
+
+number = int(input())
+odds = []
+while True:
+    if number > 0:
+        if number % 2 != 0:
+            odds.append(number)  
+            number -= 1
+        else:
+            number -= 1
+    else:
+        odds.reverse()
+        for odd in odds:
+            print(odd)
+        break
+    
---
+++ python-beecrowd/p1067-v2.py
@@ -0,0 +1,6 @@
+# Números Ímpares
+
+X = int(input())
+
+for number in range(1, X + 1, 2):
+        print(number)
